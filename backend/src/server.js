a524bcf514b80f8dc14907c469b805c5af98387d
abhishekkumar4553 <abhishekgupta45591@gmail.com>
2025-07-12 15:21:53 +0530

    🚀 Final production-ready full stack Connectify app

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,12 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -23,6 +26,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`⚙️ Server is running on port ${PORT}`);
